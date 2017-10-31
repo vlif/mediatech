bc14d147a8f404b15db70bff561b2851a9e0ee18
Vasily Lifanovsky <vlif.developer@gmail.com>
2017-10-31 21:44:26 +0300

    in the index file have only routes

--- web/index.php
+++ web/index.php
@@ -3,63 +3,18 @@ use Symfony\Component\HttpFoundation\Request;
 //use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 //use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+//use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use MiniUrl\Repository\PdoRepository;
 use MiniUrl\Service\ShortUrlService;
 
-require_once __DIR__ . '/../protected/vendor/autoload.php';
+require_once __DIR__ . '/../protected/bootstrap.php';
 
 
-$app = new Silex\Application();
-
-// Please set to false in a production environment
-$app['debug'] = true;
-
-$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
-$app['service_domain'] = $protocol . $_SERVER['SERVER_NAME'] . '/gate/';
+$app = new Vlif\Urlshortener\Application();
 
 
-$pdo = new PDO('mysql:host=localhost;dbname=url_shortener', 'root', '');
-
-
-// main error handler
-$app->error(function (\Exception $e, Request $request) use ($app) {
-    if ($e instanceof NotFoundHttpException) {
-        return (new JsonResponse)->setData(
-            array('error' => array(
-                'message' => "The requested page could not be found ('{$request->getRequestUri()}')."
-            ),
-        ));
-    }
-
-//    $code = ($e instanceof HttpException) ? $e->getStatusCode() : 500;
-    return (new JsonResponse)->setData(
-        array('error' => array(
-            'message' => 'We are sorry, but something went terribly wrong.'
-        )
-    ));
-});
-
-
-// redis
-$app->register(new Moust\Silex\Provider\CacheServiceProvider(), array(
-    'cache.options' => array(
-        'driver' => 'redis'
-    )
-));
-
-
-// json
-$app->before(function (Request $request) {
-    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-        $data = json_decode($request->getContent(), true);
-        $request->request->replace(is_array($data) ? $data : array());
-    }
-});
-
-
-// routes
+// Routes
 $app->post('', function (Request $request) use ($app, $pdo) {
         $longUrl = $request->request->get('url');
 
@@ -70,7 +25,7 @@ $app->post('', function (Request $request) use ($app, $pdo) {
                 throw new \Exception('Empty url.', 400);
             }
 
-            $service = new ShortUrlService($app['service_domain'], new PdoRepository($pdo));
+            $service = new ShortUrlService($app['service_domain'], new PdoRepository($app['pdo']));
             $url = $service->shorten($longUrl);
 
             $response->setData(array('short_url' => $url->getShortUrl()));
@@ -91,7 +46,7 @@ $app->post('', function (Request $request) use ($app, $pdo) {
 $app->get('/gate/{shortUrl}', function ($shortUrl) use ($app, $pdo) {
         $response = new JsonResponse();
 
-        // redis
+        // Redis
         $isRedisAvailable = false;
         try {
             $cachedUrl = $app['cache']->fetch($app['service_domain'] . $shortUrl);
@@ -102,18 +57,21 @@ $app->get('/gate/{shortUrl}', function ($shortUrl) use ($app, $pdo) {
             }
         } catch(\Exception $e) {
 
-            // здесь отправляем уведомление администратору о том, что redis приболел
+            //
+            // Here are sent the notice to the administrator that redis is sick
+            //
 
         }
 
         try {
-            $service = new ShortUrlService($app['service_domain'], new PdoRepository($pdo));
+            $service = new ShortUrlService($app['service_domain'], new PdoRepository($app['pdo']));
             $url = $service->expand($shortUrl);
 
             if (!is_object($url)) {
                 throw new \Exception('Short url not found', 404);
             }
 
+            // Redis
             if ($isRedisAvailable) {
                 $app['cache']->store($app['service_domain'] . $shortUrl, $url->getLongUrl(), 3600); // 3600 ttl
             }
@@ -131,8 +89,7 @@ $app->get('/gate/{shortUrl}', function ($shortUrl) use ($app, $pdo) {
     }
 );
 
-
-// main page
+// Main page
 $app->get('', function () use ($app) {
 
     return 'URL Shortener (ver 1.0)';
